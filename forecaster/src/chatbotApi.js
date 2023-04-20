c8878aa9e45e68216f146fbc8eadf19262c493e0
Marcus Carlbom <112480259+MarcusCarlbom@users.noreply.github.com>
2023-04-20 23:41:03 +0200

    ChatbotApi javascript file created and still needs method

---
+++ forecaster/src/chatbotApi.js
@@ -0,0 +1,7 @@
+function sendToChatbotAPI(text) {
+  // make API call to chatbot API with user input text
+  // process response from chatbot API
+  // return processed response
+}
+
+export { sendToChatbotAPI };
