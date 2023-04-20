3f82f40501ffef42a8a8b76652c57628b82a984a
Marcus Carlbom <112480259+MarcusCarlbom@users.noreply.github.com>
2023-04-21 01:28:30 +0200

    Delete weatherapi.js
    Added on wrong branch

--- forecaster/src/weatherapi.js
+++
@@ -1,6 +0,0 @@
-function getWeather(location, time) {
-  const currentTime = new Date().toLocaleTimeString();
-  console.log(`The weather in ${location} at ${time} is ${currentTime}.`);
-}
-
-module.exports = { getWeather };
