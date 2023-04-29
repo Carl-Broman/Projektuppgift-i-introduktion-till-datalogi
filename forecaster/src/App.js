81cef44fac2b33579f29661f7ff9646b70a509be
root <root@CarlBroman.localdomain>
2023-04-30 00:41:20 +0200

    added some front end

--- forecaster/src/App.js
+++ forecaster/src/App.js
@@ -1,28 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
-
-// userOutputManager(location, time) (Assume location is name of place)
+// import logo from './path/to/image.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Input value:', inputValue);
+        // Perform any necessary actions with the input value
+    };
+//                <img src={logo} className="App-logo" alt="logo" />
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Forecaster</h1>
+                <h2>Enter Location and time to get the weather</h2>
+                <label htmlFor="forecast-date">Date:</label>
+                <input type="date" id="forecast-date" name="forecast-date" />
+
+                <label htmlFor="forecast-time">Time:</label>
+                <input type="time" id="forecast-time" name="forecast-time" />
+
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        value={inputValue}
+                        onChange={handleChange}
+                        placeholder="Enter location"
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+            </header>
+        </div>
+    );
 }
 
 export default App;
