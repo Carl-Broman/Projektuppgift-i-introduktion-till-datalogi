d41d6199b9753f2da2ca650983871110704884b5
MarcusCarlbom <marcus@carlbom.cc>
2023-05-03 22:43:29 +0200

    Fixed input bug and call to weatherFetcher

--- forecaster/src/App.js
+++ forecaster/src/App.js
@@ -1,49 +1,81 @@
 import React, { useState } from 'react';
 import './App.css';
+import './weatherFetcher.js';
+
+const weatherFetcher = require ("./weatherFetcher");
 
 function App() {
-    const [inputValue, setInputValue] = useState('');
+  const [formData, setFormData] = useState({
+    date: '',
+    time: '',
+    location: '',
+  });
 
-    const handleChange = (event) => {
-        setInputValue(event.target.value);
-    };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name, value);
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value
+    }));
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log('Input value:', inputValue);
-        // Perform any necessary actions with the input value
-    };
+  async function forecastCollecter(date, time, location){
+    const weatherResponse = await weatherFetcher.weatherData(date, time, location)
+    console.log(weatherResponse);
+  }
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Forecaster</h1>
-                <h2>Enter required imformation to forecast weather</h2>
-                <div className="input-group">
-                    <label htmlFor="forecast-date">Date:</label>
-                    <input type="date" id="forecast-date" name="forecast-date" required />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="forecast-time">Time:</label>
-                    <input type="time" id="forecast-time" name="forecast-time" required />
-                </div>
-                <form onSubmit={handleSubmit}>
-                    <div className="input-group">
-                        <label htmlFor="forecast-location">Location:</label>
-                        <input
-                            type="text"
-                            id="forecast-location"
-                            value={inputValue}
-                            onChange={handleChange}
-                            placeholder="Enter location"
-                            required
-                        />
-                    </div>
-                    <button type="submit">Submit</button>
-                </form>
-            </header>
-        </div>
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Input value:', formData);
+    const {date, time, location } = formData;
+    forecastCollecter(date, time, location);
+    // Perform any necessary actions with the input value
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Forecaster</h1>
+        <h2>Enter required imformation to forecast weather</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="forecast-date">Date:</label>
+            <input
+              type="date"
+              id="forecast-date"
+              value={formData.date}
+              onChange={handleChange}
+              name="date"
+              required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="forecast-time">Time:</label>
+            <input
+              type="time"
+              id="forecast-time"
+              value={formData.time}
+              onChange={handleChange}
+              name="time"
+              required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="forecast-location">Location:</label>
+            <input
+              type="text"
+              id="forecast-location"
+              value={formData.location}
+              onChange={handleChange}
+              placeholder="Enter location"
+              name="location"
+              required
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </header>
+    </div>
+  );
 }
 
 export default App;
