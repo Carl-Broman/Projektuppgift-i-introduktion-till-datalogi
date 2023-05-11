49a534858ccd5f6ab65dda7970900ab87fa5e825
MarcusCarlbom <marcus@carlbom.cc>
2023-05-11 21:14:16 +0200

    Fixed parameter again

--- forecaster/src/chatgpt.js
+++ forecaster/src/chatgpt.js
@@ -9,7 +9,7 @@ export async function chatMessage(weather, location){
     const weatherQuestion = `Answer as if you are aware of the weather in ${location} as it is ${weather.weather.description} and ${weather.main.temp-272.15} degrees celsius. Write the information as if the user does not know and that you knew it before and recommend things to do there`
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{role: "user", content: `Answer as if you are aware of the weather in ${Stockholm} as it is ${weather.main.temp-272.15} celsius and ${weather.weather.description}. Write the information as if the user does not know and that you knew it before and recommend things to do there`}],
+        messages: [{role: "user", content: `Answer as if you are aware of the weather in ${location} as it is ${weather.main.temp-272.15} celsius and ${weather.weather.description}. Write the information as if the user does not know and that you knew it before and recommend things to do there`}],
       });
 
       return(completion.data.choices[0].message.content);
