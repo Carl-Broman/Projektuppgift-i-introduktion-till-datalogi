956e2057ffde8375702f0996897a2fc85871d6f7
MarcusCarlbom <marcus@carlbom.cc>
2023-05-11 21:18:35 +0200

    Fixed so that it takes the right weather description

--- forecaster/src/dalle.js
+++ forecaster/src/dalle.js
@@ -10,15 +10,16 @@ const openai = new OpenAIApi(configuration);
  * @param {*} weatherResponse 
  */
 export async function dallePicture(weatherResponse, location) {
-    console.log(process.env.REACT_APP_OPENAI_API_KEY);
+    console.log(process.env.REACT_APP_OPENAI_API_KEY)
     try {
         const openai = new OpenAIApi(configuration);
         const response = await openai.createImage({
-            prompt: `Realistic view of ${location} and the weather is ${weatherResponse.weather.description}`,
+            prompt: `Realistic view of ${location} and the weather is ${weatherResponse.weather[0].description}`,
             n: 1,
             size: "256x256",
         });
         console.log(response);
+        console.log(weatherResponse.weather.description);
         return response.data.data[0].url;
     }
     catch(err){
